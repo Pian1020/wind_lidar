237c587e8db7a8c24298e60bbec822935812a442
tinf <ericding1213@gmail.com>
2024-01-24 01:33:37 +0800

    test.py plot ok

--- test.py
+++ test.py
@@ -1,112 +1,86 @@
-
-# read files
 import os
 import pandas as pd
 from pylab import *
-
-# defines the file directory that contains the HPL files
-root = os.getcwd()
-file_dir = root + "\ProcessedData"
-
-# creates an empty list to store the DataFrames
-df_list = []
-
-# loop that iterates over all the files in the directory
-for file in os.listdir(file_dir):
-    # checks if the current file ends with the ".hpl"
-    if file.endswith(".hpl"):
-        # Extracts the date and time information from the filename
-        date_time_str = file.split("_")[-2] + "_" + file.split("_")[-1].split(".")[0]
-        
-        # opens the current file and reads all the lines
-        with open(os.path.join(file_dir, file), "r") as f:
-            # removes the first line
-            lines = f.readlines()
-            lines = lines[1:]
-            
-            # splits each line into three elements
-            data = [line.split() for line in lines]
-            
-            # creates a DataFrame from the three elements
-            df = pd.DataFrame(data, columns=["height", "wind_direction", "wind_speed"])
-            # adds the datetime to the list
-            df["datetime"]=date_time_str
-            # adds the DataFrame to the list
-            df_list.append(df)
-
-# 將df_list轉換為DataFrame
-df_multi = pd.concat(df_list)
-
-# print DataFrame
-print(df_multi)
-
-df_multi['height'] = df_multi['height'].astype(int)
-df_multi['wind_direction'] = df_multi['wind_direction'].astype(float)
-df_multi['wind_speed'] = df_multi['wind_speed'].astype(float)
-df_multi['datetime'] = df_multi['datetime'].astype(str)
-
-
-df_2000=df_multi.loc[df_multi["height"] <= 2000]
-
-height_list=[]
-height_list=df_multi['height'].values.tolist()
-count = len(os.listdir(file_dir))
-
-# Create the empty list to store wind_speed lists
-H = []
-WS = []
-DT = []
-
-# Iterate through each unique datetime in the DataFrame
-a=0
-for datetime_value in df_2000['datetime'].unique():
-    # Select wind_speed values for the current datetime
-    wind_speed_list = df_2000[df_2000['datetime'] == datetime_value]['wind_speed'].tolist()
-    # Add the extracted wind_speed list to the WS list
-    WS.append(wind_speed_list)
-    DT.append(datetime_value)
-    if a == 0:
-        H = df_2000[df_2000['datetime'] == datetime_value]['height'].tolist()
-    a=1
-
-# Rotate WS 90 degrees counterclockwise
-WS_rotated = np.array(WS).T  # Transpose and reverse lists
-
-
-# %%
-
 import matplotlib.pyplot as plt
+import numpy as np
+
+def createDataset(path):
+    # creates an dictionary to hold data <string, list>
+    dataset = []
+    time_stamps = []
+
+    # loop that iterates over all the files in the directory
+    for file in os.listdir(path):
+        # checks if the current file ends with the ".hpl"
+        if file.endswith(".hpl"):
+            # Extracts the date and time information from the filename
+            date_time_str = file.split("_")[-2] + "_" + file.split("_")[-1].split(".")[0]
+            time_stamps.append(date_time_str)
+            # opens the current file and reads all the lines
+            with open(os.path.join(path, file), "r") as f:
+                # removes the first line
+                lines = f.readlines()
+                lines = lines[1:]
+                
+                # splits each line into three elements
+                data = np.array([line.split() for line in lines])
+                dataset.append(data)
+    return np.array(dataset), time_stamps
+
+def dataSetToFloat(dataset):
+    for iter in range(len(dataset)):
+        for it in range(len(dataset[iter])):
+            for i in range(len(dataset[iter][it])):
+                dataset[iter][it][i] = float(dataset[iter][it][i])
+    return dataset
+
+def draw(dataset, count, time_stamps):
+    # 製作jet色彩映射
+    c_map = plt.cm.jet
+
+    # 將起始和結束位置固定為白色和黑色
+    c_map.set_under('white')
+    c_map.set_over('black')
 
 
-# 製作jet色彩映射
-cmap = plt.cm.jet
+    plt.figure(figsize=(12, 6))
+    # 設定橫軸
+    plt.xlabel("datetime")
+    Z = []
+    for iter in dataset:
+        Z.append(iter.T[1].astype(float))
+    X = np.arange(1, count+1, 1)
+    Y = iter.T[0].astype(float)
+    xx, yy = np.meshgrid(X, Y)
+    # 設定縱軸
+    plt.ylabel("height")
+    Z = np.array(Z)
+    # 繪製風速資料
+    for i in range(count):
+        plt.pcolormesh(xx, yy, Z.T, cmap=c_map)
+    #plt.gca().set_aspect('equal')  # Ensure equal aspect ratio for clarity
 
-# 將起始和結束位置固定為白色和黑色
-cmap.set_under('white')
-cmap.set_over('black')
+    # 設定colorbar
+    cbar = plt.colorbar(label='Wind Speed')
+    plt.clim(0, 15)
 
+    # 顯示圖表
+    plt.show()
 
-plt.figure(figsize=(12, 6))
+if __name__ == "__main__":
 
-# 設定橫軸
-plt.xlabel("datetime")
+    # defines the file directory that contains the HPL files
+    root = os.getcwd()
+    file_path = root + "/ProcessedData"
 
-# 設定縱軸
-plt.ylabel("height")
+    # Read file from the disk
+    dataset, time_stamps = createDataset(file_path)
+    print(dataset.shape)
 
-# 繪製風速資料
-for i in range(count):
-    plt.pcolormesh(WS_rotated, cmap=cmap)
-#plt.gca().set_aspect('equal')  # Ensure equal aspect ratio for clarity
+    # Modify the all elements in the dictionary into numbers from string
+    dataset = dataSetToFloat(dataset)
+    
+    number_of_data = len(os.listdir(file_path))
 
-# 設定colorbar
-cbar = plt.colorbar(label='Wind Speed')
-plt.clim(0, 15)
-
-#plt.yticks(H)
-
-# 顯示圖表
-plt.show()
-
-
-# %%
+    draw(dataset, number_of_data, time_stamps)
+    
