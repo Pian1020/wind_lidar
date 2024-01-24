1e7b95aea91b6073df40db27e07407ae59532201
tinf <ericding1213@gmail.com>
2024-01-24 10:24:52 +0800

    image accurate

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pylab import *
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
 
 def createDataset(path):
     # creates an dictionary to hold data <string, list>
@@ -34,30 +35,43 @@ def dataSetToFloat(dataset):
                 dataset[iter][it][i] = float(dataset[iter][it][i])
     return dataset
 
+def make_color_map(start_color, end_color, num_steps):
+    r = np.linspace(start_color[0], end_color[0], num_steps)
+    g = np.linspace(start_color[1], end_color[1], num_steps)
+    b = np.linspace(start_color[2], end_color[2], num_steps)
+    return np.column_stack((r, g, b))
+
 def draw(dataset, count, time_stamps):
-    # 製作jet色彩映射
-    c_map = plt.cm.jet
+    cmap1 = make_color_map([1, 1, 1], [0.92, 0.92, 0.92], 5)
+    cmap2 = make_color_map([0.92, 0.92, 0.92], [0.460, 0.829, 1], 20)
+    cmap3 = make_color_map([0.460, 0.829, 1], [0.316, 1, 0.316], 20)
+    cmap4 = make_color_map([0.316, 1, 0.316], [1, 1, 0], 20)
+    cmap5 = make_color_map([1, 1, 0], [1, 0, 0], 20)
 
+    # Concatenate the colormaps
+    c_map = np.vstack((np.ones((3, 3)), cmap1, cmap2, cmap3, cmap4, cmap5, np.zeros((3, 3))))
+    custom_cmap = LinearSegmentedColormap.from_list('custom_cmap', c_map, N=256)
     # 將起始和結束位置固定為白色和黑色
-    c_map.set_under('white')
-    c_map.set_over('black')
-
-
+    # c_map.set_under('white')
+    # c_map.set_over('black')
     plt.figure(figsize=(12, 6))
+    
     # 設定橫軸
     plt.xlabel("datetime")
     Z = []
     for iter in dataset:
-        Z.append(iter.T[1].astype(float))
+        Z.append(iter.T[2].astype(float)[:116])
     X = np.arange(1, count+1, 1)
-    Y = iter.T[0].astype(float)
+    Y = iter.T[0].astype(float)[:116]
     xx, yy = np.meshgrid(X, Y)
     # 設定縱軸
     plt.ylabel("height")
     Z = np.array(Z)
+    print(Z)
+
     # 繪製風速資料
-    for i in range(count):
-        plt.pcolormesh(xx, yy, Z.T, cmap=c_map)
+    # for i in range(count):
+    plt.pcolormesh(xx, yy, Z.T, cmap=custom_cmap)
     #plt.gca().set_aspect('equal')  # Ensure equal aspect ratio for clarity
 
     # 設定colorbar
@@ -66,6 +80,8 @@ def draw(dataset, count, time_stamps):
 
     # 顯示圖表
     plt.show()
+
+    plt.savefig('wind.png')
 
 if __name__ == "__main__":
 
